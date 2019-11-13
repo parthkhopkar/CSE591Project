fb9195e6f539e2a9b8154b70fc82289b995e1d64
Parth Khopkar <parth.khopkar96@gmail.com>
2019-11-13 13:09:45 -0700

    Created update functions for S and D grids

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import numpy as np
 import math
 
+def observation(pose, env):
+    dim = env.shape
+    x, y = pose
+    r1, r2, c1, c2 = 0, 0, 0, 0
+    if x-1 < 0:
+        c1 = 0
+    else:
+        c1 = x-1
+    if x+2 > dim[0]:
+        c2 = dim[0]
+    else:
+        c2 = x+2
+
+    if y-1 < 0:
+        r1 = 0
+    else:
+        r1 = y-1
+    if y+2 > dim[1]:
+        r2 = dim[0]
+    else:
+        r2 = y+2
+
+    return env[r1:r2, c1:c2]
+
 def update_static_grid(S, inv_sensor_model):
     prev_S = S.copy()
     S_update = np.zeros(S.shape)
@@ -8,6 +32,7 @@ def update_static_grid(S, inv_sensor_model):
         c = math.e ** math.log(inv_sensor_model / (1 - inv_sensor_model)) + math.log(prev_S[x, y] / (1 - prev_S[x, y]))
         S_update[x, y] = c/(1 + c)
     return S_update
+
 
 def update_dynamic_grid(D, inv_sensor_model):
     prev_D = D.copy()
@@ -27,13 +52,19 @@ if __name__ == "__main__":
     # print('D:', D)
     env[0, dim[1]-1] = 1
     env[1, 1] = 1
-    # print(env)
+    print(env)
 
     # Static map update
     inv_sensor_S = 0.9
     S = update_static_grid(S, inv_sensor_S)
-    print(S)
+    # print('Static grid after update')
+    # print(S)
 
     # Dynamic map update
-    inv_sensor_D = 0.9
+    inv_sensor_D = 0.1
     D = update_dynamic_grid(D, inv_sensor_D)
+    # print('Dynamic grid after update')
+    # print(D)
+
+    print(observation((0,1), env))
+
