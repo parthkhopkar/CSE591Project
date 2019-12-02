fdbcceba07a3e384c658c2a373eec646f630dd7f
Parth Khopkar (Student) <pkhopkar@EN4146272L.fulton.ad.asu.edu>
2019-12-01 19:04:39 -0700

    add ekf slam to robot

--- main.py
+++ main.py
@@ -15,96 +15,202 @@ def get_observation(pose, env):
     c2 = y + 3
     return env1[r1:r2, c1:c2]
 
+def runMapping(typ="MULTI_ROBOT"):
+	if(typ == 'MULTI_ROBOT'):
+		"""
+			2 robot test in 10X10 world
+		"""
+		# Load world
+		with open('./worlds/10X10.json') as file:
+			maze = json.load(file)
+			# Initialize maze
+			dim = [maze['dim1'], maze['dim2']]
+			world = OccupancyGrid(True, dim[0], dim[1])
+			# Initialize robots
+			robots = []
+			for i in range(maze['n']):
+				name = i + 3
+				robots.append(Robot(name, maze['r' + str(i)], dim))
+				world.update_robot_position(name, maze['r' + str(i)], maze['r' + str(i)])
+			# Initialize static objects
+			for obj in maze['static']:
+				world.add_static_object(obj[0], obj[1])
+			# Initialize Dynamic Objects
+			for obj in maze['dynamic']:
+				print(obj)
+				world.add_dynamic_object(obj[0][0], obj[0][1], obj[1][0], obj[1][1])
+			# Update poses
+			for x in range(10):
+				if x is 2:
+					world.step()
+				for a0, a1 in itertools.zip_longest(maze['r0_actions'], maze['r1_actions']):
+					world.show()
+					if a0:
+						robots[0].setObs(get_observation(robots[0].step('o'), world))
+						old_pose = robots[0].get_position().copy()
+						new_pose = robots[0].step(a0)
+						world.update_robot_position(robots[0].name, old_pose, new_pose)
+					if a1:
+						robots[1].setObs(get_observation(robots[1].step('o'), world))
+						old_pose = robots[1].get_position().copy()
+						new_pose = robots[1].step(a1)
+						world.update_robot_position(robots[1].name, old_pose, new_pose)
+				r0 = (robots[0].S.get_arr().copy(), robots[0].D.get_arr().copy(), robots[0].T.get_arr().copy())
+				robots[0].merge(robots[1].S.get_arr(), robots[1].D.get_arr(), robots[1].T.get_arr())
+				robots[1].merge(r0[0], r0[1], r0[2])
+
+			world.show()
+			np.set_printoptions(precision=1, suppress=True)
+			print('Static Occupancy Grid for R0)')
+			print(robots[0].S.get_arr())
+			print('Dynamic Occupancy Grid for R0)')
+			print(robots[0].D.get_arr())
+			print('Time Map for R0)')
+			print(robots[0].T.get_arr())
+			print('Static Occupancy Grid for R1)')
+			print(robots[1].S.get_arr())
+			print('Dynamic Occupancy Grid for R1)')
+			print(robots[1].D.get_arr())
+			print('Time Map for R1)')
+			print(robots[1].T.get_arr())
+	else:
+		"""
+		Single robot test
+		"""
+		dim = [4, 4]
+		env = OccupancyGrid()  # Array for the environment
+		env.add_static_object(2, 1)
+		env.add_static_object(3, 3)
+		env.add_dynamic_object(2, 3, 1, 3)
+		start_pose = [3, 0]
+		name = 3
+		R1 = Robot(name, start_pose, dim)
+		env.update_robot_position(name, start_pose, start_pose)
+		# env.show()
+		actions = ['r', 'r', 'u', 'u', 'u', 'l', 'l', 'd', 'd', 'd']
+		for x in range(10):
+		    for action in actions:
+		        R1.setObs(get_observation(R1.step('o'), env))
+		        # print('Static Occupancy Grid')
+		        # print(R1.S.get_arr())
+		        # print('Dynamic Occupancy Grid')
+		        # print(R1.D.get_arr())
+		        old_pose = R1.get_position().copy()
+		        if old_pose[1] == 0 and old_pose[0] == 1 and x == 2:
+		            env.step()
+		            env.show()
+		        new_pose = R1.step(action)
+		        env.update_robot_position(R1.name, old_pose, new_pose)
+		np.set_printoptions(precision=4)
+		print('Static Occupancy Grid')
+		print(np.round(R1.S.get_arr()))
+		print('Dynamic Occupancy Grid')
+		print(np.round(R1.D.get_arr()))
+
+
+def runEKFSLAM():
+	np.random.seed(42)
+
+	# EKF state covariance
+	Cx = np.diag([0.5, 0.5, np.deg2rad(30.0)]) ** 2
+
+	#  Simulation parameter
+	Q_sim = np.diag([0.2, np.deg2rad(1.0)]) ** 2
+	R_sim = np.diag([1.0, np.deg2rad(10.0)]) ** 2
+
+	DT = 0.1  # time tick [s]
+	SIM_TIME = 150.0  # simulation time [s]
+	MAX_RANGE = 20.0  # maximum observation range
+	M_DIST_TH = 2.0  # Threshold of Mahalanobis distance for data association.
+	STATE_SIZE = 3  # State size [x,y,yaw]
+	LM_SIZE = 2  # LM state size [x,y]
+
+	show_animation = True
+
+	print(__file__ + " start!!")
+
+	time = 0.0
+
+    # RFID positions [x, y]
+    RFID = np.array([[10.0, -2.0],
+                     [15.0, 10.0],
+                     [3.0, 15.0],
+                     [-5.0, 20.0]])
+
+    # State Vector [x y yaw v]'
+    xEst = np.zeros((STATE_SIZE, 1))
+    xTrue = np.zeros((STATE_SIZE, 1))
+    PEst = np.eye(STATE_SIZE)
+
+    xDR = np.zeros((STATE_SIZE, 1))  # Dead reckoning
+
+    # history
+    hxEst = xEst
+    hxTrue = xTrue
+    hxDR = xTrue
+    ts = 0
+	robot = Robot()
+    while SIM_TIME >= time:
+        ts += 1
+        time += DT
+#         print(time)
+        if time > 60:
+            #print('LM changed')
+            RFID = np.array([[10.0, -2.0],
+                     [15.0, 10.0],
+                     [15.0, 15.0],
+                     [-10.0, 20.0]])
+     	       
+        u = robot.calc_input()
+
+        xTrue, z, xDR, ud, dObj = robot.observation(xTrue, xDR, u, RFID, time)
+                
+        xEst, PEst = robot.ekf_slam(xEst, PEst, ud, z, dObj)
+        
+        #print(len(xEst))
+            
+
+        x_state = xEst[0:STATE_SIZE]
+
+        # store data history
+        hxEst = np.hstack((hxEst, x_state))
+        hxDR = np.hstack((hxDR, xDR))
+        hxTrue = np.hstack((hxTrue, xTrue))
+        
+        errorX = hxTrue[0, :] - hxEst[0, :]
+        errorY = hxTrue[1, :] - hxEst[1, :]
+        
+        error = np.sum(np.sqrt(errorX**2 + errorY**2)) / ts
+        
+        #print(error)
+
+        if show_animation:  # pragma: no cover
+            plt.cla()
+
+            plt.plot(RFID[:, 0], RFID[:, 1], "*k")
+            plt.plot(xEst[0], xEst[1], ".r")
+
+            # plot landmark
+            for i in range(calc_n_lm(xEst)):
+                plt.plot(xEst[STATE_SIZE + i * 2],
+                         xEst[STATE_SIZE + i * 2 + 1], "xg")
+
+            plt.plot(hxTrue[0, :],
+                     hxTrue[1, :], "-b")
+#             plt.plot(hxDR[0, :],
+#                      hxDR[1, :], "-k")
+            plt.plot(hxEst[0, :],
+                     hxEst[1, :], "-r")
+            
+            time_text = plt.text(0.02, 0.95, '')
+            
+            
+            time_text.set_text('err = %.1f' % error)
+            
+            plt.axis("equal")
+            plt.grid(True)
+            plt.pause(0.001)
+
 if __name__ == "__main__":
-    """
-        2 robot test in 10X10 world
-    """
-    # Load world
-    with open('./worlds/10X10.json') as file:
-        maze = json.load(file)
-        # Initialize maze
-        dim = [maze['dim1'], maze['dim2']]
-        world = OccupancyGrid(True, dim[0], dim[1])
-        # Initialize robots
-        robots = []
-        for i in range(maze['n']):
-            name = i + 3
-            robots.append(Robot(name, maze['r' + str(i)], dim))
-            world.update_robot_position(name, maze['r' + str(i)], maze['r' + str(i)])
-        # Initialize static objects
-        for obj in maze['static']:
-            world.add_static_object(obj[0], obj[1])
-        # Initialize Dynamic Objects
-        for obj in maze['dynamic']:
-            print(obj)
-            world.add_dynamic_object(obj[0][0], obj[0][1], obj[1][0], obj[1][1])
-        # Update poses
-        for x in range(10):
-            if x is 2:
-                world.step()
-            for a0, a1 in itertools.zip_longest(maze['r0_actions'], maze['r1_actions']):
-                world.show()
-                if a0:
-                    robots[0].setObs(get_observation(robots[0].step('o'), world))
-                    old_pose = robots[0].get_position().copy()
-                    new_pose = robots[0].step(a0)
-                    world.update_robot_position(robots[0].name, old_pose, new_pose)
-                if a1:
-                    robots[1].setObs(get_observation(robots[1].step('o'), world))
-                    old_pose = robots[1].get_position().copy()
-                    new_pose = robots[1].step(a1)
-                    world.update_robot_position(robots[1].name, old_pose, new_pose)
-            r0 = (robots[0].S.get_arr().copy(), robots[0].D.get_arr().copy(), robots[0].T.get_arr().copy())
-            robots[0].merge(robots[1].S.get_arr(), robots[1].D.get_arr(), robots[1].T.get_arr())
-            robots[1].merge(r0[0], r0[1], r0[2])
-
-        world.show()
-        np.set_printoptions(precision=1, suppress=True)
-        print('Static Occupancy Grid for R0)')
-        print(robots[0].S.get_arr())
-        print('Dynamic Occupancy Grid for R0)')
-        print(robots[0].D.get_arr())
-        print('Time Map for R0)')
-        print(robots[0].T.get_arr())
-        print('Static Occupancy Grid for R1)')
-        print(robots[1].S.get_arr())
-        print('Dynamic Occupancy Grid for R1)')
-        print(robots[1].D.get_arr())
-        print('Time Map for R1)')
-        print(robots[1].T.get_arr())
-
-    """
-    Single robot test
-    """
-    # dim = [4, 4]
-    # env = OccupancyGrid()  # Array for the environment
-    # env.add_static_object(2, 1)
-    # env.add_static_object(3, 3)
-    # env.add_dynamic_object(2, 3, 1, 3)
-    # start_pose = [3, 0]
-    # name = 3
-    # R1 = Robot(name, start_pose, dim)
-    # env.update_robot_position(name, start_pose, start_pose)
-    # # env.show()
-    # actions = ['r', 'r', 'u', 'u', 'u', 'l', 'l', 'd', 'd', 'd']
-    # for x in range(10):
-    #     for action in actions:
-    #         R1.setObs(get_observation(R1.step('o'), env))
-    #         # print('Static Occupancy Grid')
-    #         # print(R1.S.get_arr())
-    #         # print('Dynamic Occupancy Grid')
-    #         # print(R1.D.get_arr())
-    #         old_pose = R1.get_position().copy()
-    #         if old_pose[1] == 0 and old_pose[0] == 1 and x == 2:
-    #             env.step()
-    #             env.show()
-    #         new_pose = R1.step(action)
-    #         env.update_robot_position(R1.name, old_pose, new_pose)
-    # np.set_printoptions(precision=4)
-    # print('Static Occupancy Grid')
-    # print(np.round(R1.S.get_arr()))
-    # print('Dynamic Occupancy Grid')
-    # print(np.round(R1.D.get_arr()))
-
-
+	runEKFSLAM()
 
--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@ from occupancy_grid import OccupancyGrid
 
 class Robot(object):
 
-    def __init__(self, name, coord, dims):
+    def __init__(self, name=0, coord=(1,1), dims=[3,3]):
         self.time = 0
         self.pos = coord
         self.name = name
@@ -54,6 +54,186 @@ class Robot(object):
             self.T.update(self.time, pose)
 
         self.update_time()
+
+	def ekf_slam(self, xEst, PEst, u, z, dObj):
+		# Predict
+		S = self.STATE_SIZE
+		xEst[0:S] = self.motion_model(xEst[0:S], u)
+		G, Fx = self.jacob_motion(xEst[0:S], u)
+		PEst[0:S, 0:S] = G.T * PEst[0:S, 0:S] * G + Fx.T * Cx * Fx
+		initP = np.eye(2)
+		print(z)
+		# Update
+		for iz in range(len(z[:, 0])):  # for each observation
+			min_id = self.search_correspond_landmark_id(xEst, PEst, z[iz, 0:2])
+			
+			if(z[iz, 2] in dObj):
+				continue
+				
+			nLM = calc_n_lm(xEst)
+			
+			if min_id == nLM:
+				#print("New LM")
+				# Extend state and covariance matrix
+				xAug = np.vstack((xEst, self.calc_landmark_position(xEst, z[iz, :])))
+				PAug = np.vstack((np.hstack((PEst, np.zeros((len(xEst), LM_SIZE)))),
+								  np.hstack((np.zeros((LM_SIZE, len(xEst))), initP))))
+				xEst = xAug
+				PEst = PAug
+			lm = self.get_landmark_position_from_state(xEst, min_id)
+				
+			y, S, H = self.calc_innovation(lm, xEst, PEst, z[iz, 0:2], min_id)
+
+			K = (PEst @ H.T) @ np.linalg.inv(S)
+			xEst = xEst + (K @ y)
+			PEst = (np.eye(len(xEst)) - (K @ H)) @ PEst
+
+		xEst[2] = self.pi_2_pi(xEst[2])
+
+		return xEst, PEst
+
+
+	def calc_input(self):
+		v = 1.0  # [m/s]
+		yaw_rate = 0.1  # [rad/s]
+		u = np.array([[v, yaw_rate]]).T
+		return u
+
+	def getDynamicObjects(self, time):
+		if(time > 60):
+			return [2,3]
+		else:
+			return []
+
+	def observation(self, xTrue, xd, u, RFID, time):
+		xTrue = motion_model(xTrue, u)
+
+		# add noise to gps x-y
+		z = np.zeros((0, 3))
+		
+		for i in range(len(RFID[:, 0])):
+
+			dx = RFID[i, 0] - xTrue[0, 0]
+			dy = RFID[i, 1] - xTrue[1, 0]
+			d = math.sqrt(dx ** 2 + dy ** 2)
+			angle = self.pi_2_pi(math.atan2(dy, dx) - xTrue[2, 0])
+			if d <= MAX_RANGE:
+				dn = d + np.random.randn() * self.Q_sim[0, 0] ** 0.5  # add noise
+				angle_n = angle + np.random.randn() * self.Q_sim[1, 1] ** 0.5  # add noise
+				zi = np.array([dn, angle_n, i])
+				z = np.vstack((z, zi))
+		
+		dObj = self.getDynamicObjects(time)
+
+		# add noise to input
+		ud = np.array([[
+			u[0, 0] + np.random.randn() * R_sim[0, 0] ** 0.5,
+			u[1, 0] + np.random.randn() * R_sim[1, 1] ** 0.5]]).T
+
+		xd = self.motion_model(xd, ud)
+		return xTrue, z, xd, ud, dObj
+
+
+	def motion_model(self, x, u):
+		F = np.array([[1.0, 0, 0],
+					  [0, 1.0, 0],
+					  [0, 0, 1.0]])
+
+		B = np.array([[DT * math.cos(x[2, 0]), 0],
+					  [DT * math.sin(x[2, 0]), 0],
+					  [0.0, DT]])
+
+		x = (F @ x) + (B @ u)
+		return x
+
+
+	def calc_n_lm(self, x):
+		n = int((len(x) - STATE_SIZE) / LM_SIZE)
+		return n
+
+
+	def jacob_motion(self, x, u):
+		Fx = np.hstack((np.eye(STATE_SIZE), np.zeros(
+			(STATE_SIZE, LM_SIZE * calc_n_lm(x)))))
+
+		jF = np.array([[0.0, 0.0, -DT * u[0] * math.sin(x[2, 0])],
+					   [0.0, 0.0, DT * u[0] * math.cos(x[2, 0])],
+					   [0.0, 0.0, 0.0]])
+
+		G = np.eye(STATE_SIZE) + Fx.T * jF * Fx
+
+		return G, Fx,
+
+
+	def calc_landmark_position(self ,x, z):
+		zp = np.zeros((2, 1))
+
+		zp[0, 0] = x[0, 0] + z[0] * math.cos(x[2, 0] + z[1])
+		zp[1, 0] = x[1, 0] + z[0] * math.sin(x[2, 0] + z[1])
+
+		return zp
+
+
+	def get_landmark_position_from_state(self, x, ind):
+		lm = x[STATE_SIZE + LM_SIZE * ind: STATE_SIZE + LM_SIZE * (ind + 1), :]
+
+		return lm
+
+
+	def search_correspond_landmark_id(self, xAug, PAug, zi):
+		"""
+		Landmark association with Mahalanobis distance
+		"""
+
+		nLM = calc_n_lm(xAug)
+
+		min_dist = []
+
+		for i in range(nLM):
+			lm = self.get_landmark_position_from_state(xAug, i)
+			y, S, H = self.calc_innovation(lm, xAug, PAug, zi, i)
+			min_dist.append(y.T @ np.linalg.inv(S) @ y)
+
+		min_dist.append(M_DIST_TH)  # new landmark
+
+		min_id = min_dist.index(min(min_dist))
+
+		return min_id
+
+
+	def calc_innovation(self, lm, xEst, PEst, z, LMid):
+		delta = lm - xEst[0:2]
+		q = (delta.T @ delta)[0, 0]
+		z_angle = math.atan2(delta[1, 0], delta[0, 0]) - xEst[2, 0]
+		zp = np.array([[math.sqrt(q), pi_2_pi(z_angle)]])
+		y = (z - zp).T
+		y[1] = self.pi_2_pi(y[1])
+		H = jacob_h(q, delta, xEst, LMid + 1)
+		S = H @ PEst @ H.T + Cx[0:2, 0:2]
+
+		return y, S, H
+
+
+	def jacob_h(self, q, delta, x, i):
+		sq = math.sqrt(q)
+		G = np.array([[-sq * delta[0, 0], - sq * delta[1, 0], 0, sq * delta[0, 0], sq * delta[1, 0]],
+					  [delta[1, 0], - delta[0, 0], - q, - delta[1, 0], delta[0, 0]]])
+
+		G = G / q
+		nLM = self.calc_n_lm(x)
+		F1 = np.hstack((np.eye(3), np.zeros((3, 2 * nLM))))
+		F2 = np.hstack((np.zeros((2, 3)), np.zeros((2, 2 * (i - 1))),
+						np.eye(2), np.zeros((2, 2 * nLM - 2 * i))))
+
+		F = np.vstack((F1, F2))
+
+		H = G @ F
+
+		return H
+
+
+	def pi_2_pi(self, angle):
+		return (angle + math.pi) % (2 * math.pi) - math.pi
 
     def update_time(self):
         observed = np.array(self.T.get_arr() > 0, np.int)
